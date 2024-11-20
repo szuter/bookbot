048af1d1be4b70700dded4bf906db1219a4c95d3
szuter <piotrstrzelec1989@gmail.com>
2024-11-20 16:03:30 +0100

    added words counter

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def get_book_text(file_path):    
     with open(file_path) as f:
         return f.read()
+    
+def get_number_of_words(text):
+    words_list = text.split() 
+    return len(words_list)
 
     
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
-    print(text)
+    num_words = get_number_of_words(text)
+    print(num_words)
             
 main()
