783ca1634ec3c67aba94123f2fa8a7b6f734de32
szuter <piotrstrzelec1989@gmail.com>
2024-11-20 16:11:26 +0100

    added report function

--- main.py
+++ main.py
@@ -16,12 +16,31 @@ def get_chars_dict(text):
             char_dict[c] = 1
     return char_dict
 
+def sort_on(dict):
+    return dict["num"]
+
+def char_dict_to_sorted_list(char_dict):
+    sorted_list = []
+    for c in char_dict:
+        if c.isalpha():
+            sorted_list.append({"char": c, "num": char_dict[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def print_report(num_words,char_dict,file_path):
+    sorted_list = char_dict_to_sorted_list(char_dict)
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for c in sorted_list:
+        print(f"The '{c['char']}' character was found {c['num']} times")
+    print("--- End report ---") 
     
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
     num_words = get_number_of_words(text)
     char_dict = get_chars_dict(text)
-    print(char_dict)
+    print_report(num_words,char_dict,file_path)
             
 main()
